c24d02bebf96d1c1b2c06dc74be397ccafd45a5f
Alexis <mamanialexis29@gmail.com>
2025-04-09 18:15:04 -0300

    ej4 completado

--- TrabajoPractico4/Ejercicio4.asm
+++ TrabajoPractico4/Ejercicio4.asm
@@ -5,6 +5,8 @@
 ARREGLO: 	.word 5,1,12,30,10 #
 N: 		.word 5
 MINIMO: 	.word 0
+msg:        .string "El valor mínimo es: "  # Mensaje para mostrar
+
 		.text
 		.globl main
 main:
@@ -17,5 +19,33 @@ main:
 		lw t2,0(s0)	#t2 = minimo actual = ARREGLO[0]
 	loop:
 		beq t1,s1,fin	#termina si i == N
+		slli t3,t1,2 #offset
+		add t4,s0,t3 #t4 = ARREGLO[i]
+		lw t5,0(t4) #t5 = ARREGLO[i]
 		
+		#compara el minimo actual
+		bge t5,t2,no_update	#si t5>= t2 salta
+		mv t2,t5		#mueve el contenido de t5 a t2 (actualiza el minimo del arreglo)
+		
+	no_update:
+		addi t1,t1,1 	#incrementa el indice
+		j loop
+		
+	fin:
+		la t6,MINIMO 	#carga direccion del MINIMO
+		sw t2,0(t6)		#almacena el valor del minimo
+		
+		# Mostrar mensaje por terminal
+  	 	 li a7, 4             # Código de llamada al sistema para print_string
+   	 	la a0, msg           # Cargar dirección del mensaje
+		ecall
+    
+   		 # Mostrar el valor mínimo (entero)
+		li a7, 1             # Código de llamada al sistema para print_int
+   		 mv a0, t2            # Cargar el valor mínimo
+   		 ecall
+		
+		#termina programa
+		li a7,10
+		ecall
 		
